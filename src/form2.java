1ae3057dba7e74bc2c5b8c5c6d720c7362b265ee
josdank <josue.guerra@epn.edu.ec>
2024-06-28 11:03:31 -0500

    cambio 2

--- src/form2.java
+++ src/form2.java
@@ -1,5 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form2 {
     public JPanel menu2;
+    private JButton aceptar2bt;
+
+    public form2() {
+        aceptar2bt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Bienvenido al menu 3");
+                JFrame frame = new JFrame("Bienvenido");
+                frame.setContentPane(new form3().menu3);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(1024, 768);
+                frame.setPreferredSize(new Dimension(1024, 768));
+                frame.setMinimumSize(new Dimension(1024, 768));
+                frame.pack();
+                frame.setVisible(true);
+
+                // Cierra la ventana actual
+                ((JFrame) SwingUtilities.getWindowAncestor(aceptar2bt)).dispose();
+            }
+        });
+    }
 }
